90206348934e78ccf76ce6ddf265348b08482ab1
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2022-02-11 18:19:52 +0800

    20220211 Styling the Hero page

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -4,33 +4,29 @@ import { Carousel } from "react-bootstrap";
 import Carousel1 from "../../asset/images/Carousel1.jpg";
 import Carousel2 from "../../asset/images/Carousel2.jpg";
 import Carousel3 from "../../asset/images/Carousel3.jpg";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+
+const nextBtn = <IoIosArrowForward></IoIosArrowForward>;
+const prevBtn = <IoIosArrowBack></IoIosArrowBack>;
 
 const Hero = () => {
+  const Items = [
+    { title: "Innovative ", message: "Nulla vitae elit libero", image: Carousel2 },
+    { title: "Committed ", message: "Nulla vitae elit libero", image: Carousel3 },
+    { title: "Cooperative", message: "Nulla vitae elit libero", image: Carousel1 },
+  ];
+
   return (
-    <Carousel fade id="hero" className="vw-100 vh-100">
-      <Carousel.Item>
-        <img className="d-block w-100 h-100" src={Carousel1} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100 h-100" src={Carousel2} alt="Second slide" />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100 h-100" src={Carousel3} alt="Third slide" />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+    <Carousel fade id="hero" className="vw-100 vh-100" nextIcon={nextBtn} prevIcon={prevBtn}>
+      {Items.map((item) => (
+        <Carousel.Item>
+          <img className="d-block w-100 h-100" src={item.image} alt="First slide" />
+          <Carousel.Caption>
+            <h3>{item.title}</h3>
+            <p>{item.message}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 };
